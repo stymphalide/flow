5006dfab6b20d00fbd53e5e6fd58781ea2529629
stymphalide <angelo.birrer@gmail.com>
2017-12-24 02:37:14 +0100

    Added a controller module to convert from string to python values and do the communication with the game.

--- src/api.py
+++ src/api.py
@@ -1,17 +1,24 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
-
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
+import src.controller as c
 
 class SimpleEcho(WebSocket):
 
     def handleMessage(self):
-        # echo message back to client
-        self.sendMessage(self.data)
+      print(self.data)
+      json_msg = c.parser(self.data)
+      if(json_msg):
+        self.sendMessage(json_msg)
 
     def handleConnected(self):
-        print(self.address, 'connected')
+      print(self.address, 'connected')
+      print(self.address, 'send initial game')
+      self.sendMessage(c.parser('"start"'))
 
     def handleClose(self):
-        print(self.address, 'closed')
+      print(self.address, 'closed')
 
-server = SimpleWebSocketServer('', 4000, SimpleEcho)
+server = SimpleWebSocketServer('', 5000, SimpleEcho)
 server.serveforever()
