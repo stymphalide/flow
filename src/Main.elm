48f7de0f16e0a0fabe35aec8db9141766546ea7e
stymphalide <angelo.birrer@gmail.com>
2017-12-24 03:23:40 +0100

    Extended the Main.elm file such that it can receive the initial game, now extend the view part.

--- src/Main.elm
+++ src/Main.elm
@@ -1,33 +1,65 @@
 module Main exposing (..)
 
 import Html exposing (program, text)
+import WebSocket
+
+import Json.Decode as Decode
+import Json.Decode.Pipeline exposing (decode, required)
+
+type alias Model =
+    Result String Game
 
 
+type alias Game =
+    { grid : Grid Int
+    , control_grid : Grid Bool
+    , score : Int
+    , ended : Bool
+    }
 
-type Model =
-    NoState
+type Msg 
+    = NoOp
+    | GameState String
 
-
-type Msg =
-    NoOp
-
+type alias Grid a =
+    List (List a)
 
 -- UPDATE
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    (model, Cmd.none)
+    case msg of
+        NoOp ->
+            (model, Cmd.none)
+        GameState gameString ->
+            let
+                resGame =
+                    Decode.decodeString gameDecoder gameString
+            in
+                (resGame, Cmd.none)
 
 -- INIT
 init : (Model, Cmd Msg)
 init =
-    (NoState, Cmd.none)
+    (Ok initialGame, Cmd.none)
+initialGame =
+    {grid = [[0, 0], [0,0]], control_grid = [[False, False], [False, False]], score = 0, ended = False}
 
 -- VIEW
 view model =
-    text "Nothing to be seen yet."
+    text <| toString model
 
 -- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    WebSocket.listen "ws://localhost:5000/" GameState
+-- Decoder
+gameDecoder : Decode.Decoder Game
+gameDecoder =
+    decode Game
+    |> required "grid" (Decode.list (Decode.list Decode.int))
+    |> required "control_grid"  (Decode.list (Decode.list Decode.bool))
+    |> required "score" Decode.int
+    |> required "ended" Decode.bool
 
 
 -- MAIN
