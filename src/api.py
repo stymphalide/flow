04210008560880109ee9c2fee55314f84c712cc9
stymphalide <angelo.birrer@gmail.com>
2017-12-24 05:34:30 +0100

    Resolved errors with the web Socket, game now working.

--- src/api.py
+++ src/api.py
@@ -1,24 +1,22 @@
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+#!/usr/bin/env python3.5
+import asyncio
+import websockets
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
 import src.controller as c
 
-class SimpleEcho(WebSocket):
 
-    def handleMessage(self):
-      print(self.data)
-      json_msg = c.parser(self.data)
-      if(json_msg):
-        self.sendMessage(json_msg)
 
-    def handleConnected(self):
-      print(self.address, 'connected')
-      print(self.address, 'send initial game')
-      self.sendMessage(c.parser('"start"'))
+async def game(websocket, path):
+    while True:
+      data = await websocket.recv()
+      print("< {}".format(data))
+      msg = c.parser(data)
+      await websocket.send(msg)
+      print("> {}".format(msg))
 
-    def handleClose(self):
-      print(self.address, 'closed')
+start_server = websockets.serve(game, 'localhost', 5000)
 
-server = SimpleWebSocketServer('', 5000, SimpleEcho)
-server.serveforever()
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
--- src/controller.py
+++ src/controller.py
@@ -11,7 +11,15 @@ def parser(data):
 		if data['color'] != None and data['state'] != None:
 			color = data['color']
 			state = data['state']
+			state['grid'] = np.array(state['grid'])
+			state['control_grid'] = np.array(state['control_grid'])
 			game_data = g.update_game(color, state)
+			if game_data['ended']:
+				game_data['ended'] = True
+			else:
+				game_data['ended'] = False
+			game_data['grid'] = np.ndarray.tolist(game_data['grid'])
+			game_data['control_grid'] = np.ndarray.tolist(game_data['control_grid'])
 	elif type(data) == str:
 		if data == "start":
 			game_data = g.initialise_game(6, 10, 10)
